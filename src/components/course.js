149e1aaf7c8fdfeb7f344fa0583def5fef5054e0
murphy mj <2martinmurphy@gmail.com>
2019-06-09 21:39:30 +0100

    Components completed

---
+++ src/components/course.js
@@ -0,0 +1,15 @@
+import React , { Component } from 'react';
+
+export default class Module extends Component {
+    render() {
+        return (
+            <tr>
+                <td>{this.props.description.name}</td>
+                <td>{this.props.description.noLectures}</td>
+                <td>{this.props.description.noPracticals}</td>
+            </tr>
+        );
+    }
+
+}
+
